dff546b8ddfdb91e15ab76dcde84f2cece6f125c
Sai Vishal Matcha <98102123+VishaMatcha@users.noreply.github.com>
2025-04-03 16:29:07 -0700

    Time: 164 ms (21.29%), Space: 68.3 MB (24.69%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,12 +1,12 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         c = [0]*len(questions)
-        def maxout(i):
+        def most(i):
             if i >= len(questions):
                 return 0
+            reward, cost = questions[i]
             if c[i]:
                 return c[i]
-            reward, cost = questions[i]
-            c[i] =  max(maxout(i+1), reward+maxout(i+1+cost))
+            c[i] = max(most(i+1),reward+most(i+1+cost))
             return c[i]
-        return maxout(0)
+        return most(0)
