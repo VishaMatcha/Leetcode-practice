38fb752bc61b91567609390f4a7b7c70c6460aef
Sai Vishal Matcha <98102123+VishaMatcha@users.noreply.github.com>
2025-04-03 16:29:54 -0700

    Time: 141 ms (29.33%), Space: 67.3 MB (30%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -4,9 +4,9 @@ class Solution:
         def most(i):
             if i >= len(questions):
                 return 0
-            reward, cost = questions[i]
             if c[i]:
                 return c[i]
+            reward, cost = questions[i]
             c[i] = max(most(i+1),reward+most(i+1+cost))
             return c[i]
         return most(0)
