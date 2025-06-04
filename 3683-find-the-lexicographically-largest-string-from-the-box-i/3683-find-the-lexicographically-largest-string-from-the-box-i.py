9ab06b7e4b2b342d610370ece318f2a964e59883
Sai Vishal Matcha <98102123+VishaMatcha@users.noreply.github.com>
2025-06-04 13:55:33 -0700

    Time: 11 ms (81.46%), Space: 18 MB (50.33%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        n = len(word)
+        m = n - numFriends + 1
+        i = 0
+        j = 1
+        while j < n:
+            k = 0
+            while j + k < n and word[i + k] == word[j + k]:
+                k += 1
+            if j + k < n and word[i + k] < word[j + k]:
+                i, j = j, max(j + 1, i + k + 1)
+            else:
+                j += k + 1
+        s = word[i:]
+        return s if len(s) <= m else s[:m]
