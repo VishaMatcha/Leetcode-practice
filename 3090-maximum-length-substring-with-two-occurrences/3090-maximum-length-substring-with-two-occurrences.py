36d951970af6ccf31dd3786475f772d3eca5c60b
Sai Vishal Matcha <98102123+VishaMatcha@users.noreply.github.com>
2026-08-16 10:49:12 -0700

    Time: 6 ms (26.53%), Space: 19.1 MB (97.97%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -8,13 +8,11 @@ class Solution:
             char = s[right]
             freq[char] = freq.get(char, 0) + 1
             
-            # Shrink the window if a character appears more than twice
             while freq[char] > 2:
                 left_char = s[left]
                 freq[left_char] -= 1
                 left += 1
                 
-            # Record the maximum length of a valid window
             max_len = max(max_len, right - left + 1)
             
         return max_len
