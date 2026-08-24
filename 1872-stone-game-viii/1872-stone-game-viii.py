0f28658a1eb3b48ba2a44a511558f5ab8830322d
Sai Vishal Matcha <98102123+VishaMatcha@users.noreply.github.com>
2026-08-24 17:15:53 -0500

    Time: 656 ms (82.35%), Space: 33 MB (64.12%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -4,15 +4,11 @@ class Solution:
     def stoneGameVIII(self, stones: List[int]) -> int:
         n = len(stones)
         
-        # Compute prefix sums in-place
         for i in range(1, n):
             stones[i] += stones[i-1]
             
-        # Base case: choosing the last available index (taking all remaining stones)
         dp = stones[-1]
         
-        # Traverse backwards to calculate the DP
-        # We stop at 1 because a player must choose x > 1 stones (i.e., at least index 1)
         for i in range(n-2, 0, -1):
             if stones[i] - dp > dp:
                 dp = stones[i] - dp
